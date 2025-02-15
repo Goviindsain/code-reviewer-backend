1ecd26a926b37cd3d9389f0719ac3e4af10d5b74
Goviindsain <gsksain267@gmail.com>
2025-02-15 09:17:13 +0530

    changes in line position

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const aiRoutes = require('./routes/ai.routes')
 const cors = require('cors')
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin: 'https://code-reviewer-frontend.vercel.app' 
+  }))
+  
+const aiRoutes = require('./routes/ai.routes')
 
 
 app.use(express.json())
